ff42220de8f95b73a0e37f2dfdc6278dd40a24b0
Obi Zim <52291700+Obizim@users.noreply.github.com>
2020-06-10 14:41:34 -0700

    Tabbed Content

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, data){
+    var i, tabLinks, tabContent;
+
+    tabContent = document.getElementsByClassName('tabcontent');
+    for(i = 0; i < tabContent.length; i++){
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tabLinks.length; i++){
+        tabLinks[i].className = tabLinks[i].className.replace("show", "");
+    }
+
+    document.getElementById(data).style.display= "flex";
+    evt.currentTarget.className += " show";
+}
