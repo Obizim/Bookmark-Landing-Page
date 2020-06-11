dd8d97e0d8586d2629713b6d6380f3838ab081ec
Obi Zim <52291700+Obizim@users.noreply.github.com>
2020-06-10 20:34:38 -0700

    Extension Layout

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
+
+
+
+
+// TABBED SECTION JS
 document.getElementById("defaultOpen").click();
 
-function openTab(evt, data){
+function openTab(evt, data) {
     var i, tabLinks, tabContent;
 
     tabContent = document.getElementsByClassName('tabcontent');
-    for(i = 0; i < tabContent.length; i++){
+    for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
 
     tabLinks = document.getElementsByClassName('tablinks');
-    for(i = 0; i < tabLinks.length; i++){
+    for (i = 0; i < tabLinks.length; i++) {
         tabLinks[i].className = tabLinks[i].className.replace("show", "");
     }
 
-    document.getElementById(data).style.display= "flex";
+    document.getElementById(data).style.display = "flex";
     evt.currentTarget.className += " show";
 }
